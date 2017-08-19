f8173b3f5153379b132fb10c8fa62410ee79d058
Karl Heitmann <khf1988@gmail.com>
2017-08-19 19:07:11 -0300

    agregado flash messages (pero sin probarlos) y un poco de breadcrumbs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def formateo_de_fecha(objeto_datetime)
+    return objeto_datetime.localtime.strftime "%d/%m/%Y"
+  end
+  def formateo_de_fecha_y_hora(objeto_datetime)
+    return "#{objeto_datetime.localtime.strftime "%d/%m/%Y"} <br> #{objeto_datetime.localtime.strftime "%H:%M:%S"}".html_safe
+  end
 end
