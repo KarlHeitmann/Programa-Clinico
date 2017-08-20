bffa2583eb7f765d3923ee6d805dc88904e29dfe
Karl Heitmann <khf1988@gmail.com>
2017-08-20 15:01:42 -0300

    vista show paciente y diagnosticos de paciente arregladas

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,15 @@ module ApplicationHelper
   def formateo_de_fecha_y_hora(objeto_datetime)
     return "#{objeto_datetime.localtime.strftime "%d/%m/%Y"} <br> #{objeto_datetime.localtime.strftime "%H:%M:%S"}".html_safe
   end
+  def cortar_descripcion(texto)
+    unless texto.nil?
+      if texto.length > 70
+        return "#{texto[0...70]}..."
+      else
+        return texto
+      end
+    else
+      return ""
+    end
+  end
 end
