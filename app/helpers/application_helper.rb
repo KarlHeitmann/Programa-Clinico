117bf26bb6ec298e09fc3c66c9488416712e060f
Karl Heitmann <khf1988@gmail.com>
2017-08-20 15:47:23 -0300

    vistas de diagnosticos mejoradas

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,13 @@
 module ApplicationHelper
+
   def formateo_de_fecha(objeto_datetime)
     return objeto_datetime.localtime.strftime "%d/%m/%Y"
   end
+
   def formateo_de_fecha_y_hora(objeto_datetime)
     return "#{objeto_datetime.localtime.strftime "%d/%m/%Y"} <br> #{objeto_datetime.localtime.strftime "%H:%M:%S"}".html_safe
   end
+
   def cortar_descripcion(texto)
     unless texto.nil?
       if texto.length > 70
@@ -16,4 +19,9 @@ module ApplicationHelper
       return ""
     end
   end
+  def age(dob)
+    now = Time.now.utc.to_date
+    now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+  end
+
 end
